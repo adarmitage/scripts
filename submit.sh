a5efd303a512d9b44f5291eae86f54fc74379a0f
armita <Andrew.Armitage.emr.ac.uk>
2014-03-18 14:26:22 +0000

    edited range of hash lengths (41-101bp)

--- submit.sh
+++ submit.sh
@@ -4,10 +4,10 @@
 
 
 FORWARD_FILE=P.cact_411_1M_F_trim.fastq
-REVERSE_FILE=P.cact_411_1M_F_trim.fastq
+REVERSE_FILE=P.cact_411_1M_R_trim.fastq
 ASSEMBLY_NAME=P.cact.auto
 
 cd $PWD
-for HASH_LENGTH in $( seq 21 2 51 ); do
+for HASH_LENGTH in $( seq 41 2 101 ); do
 	qsub ~/scripts/align.sh $HASH_LENGTH $FORWARD_FILE $REVERSE_FILE $ASSEMBY_NAME
 done
