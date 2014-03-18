10fbf743e057a6a418b8e08905622c9f0ae7a61f
armita <Andrew.Armitage.emr.ac.uk>
2014-03-18 15:16:13 +0000

    Script to prepare whole genome sequencing data for assembly

---
+++ seq_prep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#######  Step 1	 ########
+# 	Initialise values	#
+#########################
+
+$F_READ='P.cact_411_1M_F.fastq.gz'
+$R_READ='P.cact_411_1M_R.fastq.gz'
+
+$F_TRIM=(echo $F_READ | tr -d '.gz')
+$R_TRIM=(echo $F_READ | tr -d '.gz')
+
+echo "your forward read is: $F_READ"
+echo "your reverse read is: $R_READ"
+
+echo "your trimmed forward read will be named: $F_TRIM"
+echo "your trimmed reverse read will be named: $R_TRIM"
