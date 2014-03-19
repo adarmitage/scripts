137df4b03f3505abda4efad38cbe713f82ac502b
armita <Andrew.Armitage.emr.ac.uk>
2014-03-19 18:00:44 +0000

    improved assemble.sh structure. Assembly summary files now include assembly size stats and no. conitgs stats

--- assembly_pipe.sh
+++ assembly_pipe.sh
@@ -106,7 +106,7 @@ rm $ASSEMBLY_NAME.*
 # 	Assemble	#
 #########################
 
-echo "ASSEMBLY_NAME    EXP_COV        HASH_LENGTH	N50	MAX_CONTIG" > "$ASSEMBLY_NAME"_stats.txt
+echo "ASSEMBLY_NAME    EXP_COV        HASH_LENGTH	N50	MAX_CONTIG	NO_CONITG	NO_BP" > "$ASSEMBLY_NAME"_stats.txt
 
 cd $PWD
 for HASH_LENGTH in $( seq 41 10 101 ); do
